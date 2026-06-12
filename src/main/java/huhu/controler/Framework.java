c2cb0f24692bcf1132a5ee11d3edfeb6ed509e45
nyandomm4 <nyandomm6@gmail.com>
2026-06-12 09:36:27 +0300

    Update Framework.java

--- src/main/java/huhu/controler/Framework.java
+++ src/main/java/huhu/controler/Framework.java
@@ -1,6 +1,7 @@
 package huhu.controler;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,14 +13,25 @@ public class Framework extends jakarta.servlet.http.HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
 
-        // isPost(req, res);
+        isPost(req, res);
 
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
-        // isGet(req, res);
+        isGet(req, res);
 
+    }
+
+    public void isGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        PrintWriter out = res.getWriter();
+        out.println("get");
+
+    }
+
+    public void isPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        PrintWriter out = res.getWriter();
+        out.println("post");
     }
 }
